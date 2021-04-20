f37dd25afbf12ffd0cfc0c47cc9370a164f29f8e
jay <jay@jay.localdomain>
2021-04-21 02:23:50 +0700

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <div>
+      <div className="container">Hello</div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto justify-center align-item-center mt-5">
+        <h1>Hulu</h1>
+      </div>
+    </div>
+  );
+}
