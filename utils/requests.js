b7d1bffd4b6fa72fdb64c7fddaf0cdcd22e95706
jay <jay@jay.localdomain>
2021-04-21 17:37:29 +0700

    request

--- utils/requests.js
+++ utils/requests.js
@@ -2,8 +2,8 @@ const API_KEY = process.env.API_KEY;
 
 export default {
   fetchTrending: {
-    title: "Trending",
-    url: `/trending/all/week/?api_key=${API_KEY}&language=en-US`,
+    title: "Popular",
+    url: `/movie/popular?api_key=${API_KEY}&language=en-US`,
   },
   fetchTopRated: {
     title: "Top Rated",
