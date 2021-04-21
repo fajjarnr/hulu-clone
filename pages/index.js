944f53900f29ccd78d906f65d943f0583b370826
jay <jay@jay.localdomain>
2021-04-21 16:39:43 +0700

    result

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
+
   return (
     <div>
       <Head>
@@ -12,6 +16,28 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const genre = context.query.genre;
+
+    const res = await fetch(
+      `https://api.themoviedb.org/3${
+        requests[genre]?.url ?? requests.fetchTrending.url
+      }`
+    );
+    const data = await res.json();
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
